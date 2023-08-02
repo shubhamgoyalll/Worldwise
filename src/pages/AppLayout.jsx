d14a63fe5d6d018dd7682ba8b59ac6d5a2d5765c
Shubham Goyal <goyalshubham2521@gmail.com>
2023-08-03 01:18:11 +0530

    Build AppLayout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,12 @@
-import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
+import Map from "../components/Map";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
