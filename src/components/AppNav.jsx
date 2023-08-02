7cf96b82dd10e0e08507b995b1ae4f1268817d98
Shubham Goyal <goyalshubham2521@gmail.com>
2023-08-02 15:50:35 +0530

    Build multiple pages,Added PageNav to all pages, Added Login Route

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,8 @@
+// import {nav} from "../components/AppNav.module.css"; OR
+import styles from "../components/AppNav.module.css";
+
+function AppNav() {
+  return <nav className={styles.nav}>App Navigation</nav>;
+}
+
+export default AppNav;
