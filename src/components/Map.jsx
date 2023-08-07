add9d9e7ba610cb97bc085f427757bfb389f2724
Shubham Goyal <goyalshubham2521@gmail.com>
2023-08-08 01:36:23 +0530

    Added Cities Name to All Markups in Map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,11 +2,14 @@ import { useSearchParams, useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
 import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   // hook to take states or data from the URL
   const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
+
+  const { cities } = useCities();
 
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
@@ -25,11 +28,17 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
