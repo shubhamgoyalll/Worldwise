cd93af8173278ab46bc1552e06bc200b83b5c007
shubhamgoyalll <goyalshubham2521@gmail.com>
2023-08-25 04:19:51 +0530

    Implemented Login and Logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -12,29 +12,32 @@ import City from "./components/City";
 import Form from "./components/Form";
 import { Navigate } from "react-router-dom";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* <Route path="/" element={<Homepage />} /> OR */}
-          <Route index element={<Homepage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="app" element={<AppLayout />}>
-            {/*We have used nested routes and an index route here. Now to use these we use <Outlet/> wherever we want these. And if none of these matches then index route JSX will be shown*/}
-            <Route index element={<Navigate replace to="cities" />} />
-            <Route path="cities" element={<CityList />} />
-            <Route path="cities/:id" element={<City />} />
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-          </Route>
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* <Route path="/" element={<Homepage />} /> OR */}
+            <Route index element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="app" element={<AppLayout />}>
+              {/*We have used nested routes and an index route here. Now to use these we use <Outlet/> wherever we want these. And if none of these matches then index route JSX will be shown*/}
+              <Route index element={<Navigate replace to="cities" />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
 
--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -30,7 +30,7 @@ const FAKE_USER = {
   name: "Shubham",
   email: "shubham@example.com",
   password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
+  avatar: "../../public/myimage.JPG",
 };
 
 function AuthProvider({ children }) {
